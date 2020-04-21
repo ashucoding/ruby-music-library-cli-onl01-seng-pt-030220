9acce54df99399023086a351629a1b3c96f4fcb3
Shana Wiltshire <shanawiltshire@gmail.com>
2020-04-21 16:08:49 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,4 +3,9 @@ module Finable
   def find_by_name(name)
     self.all.detect{|o| o.name == name}
   end
+  
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+  
 end
