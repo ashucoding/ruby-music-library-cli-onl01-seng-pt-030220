9ca2e3a786267d8fc085e37fcbc350e7f3392dc5
Shana Wiltshire <shanawiltshire@gmail.com>
2020-04-21 19:37:08 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ Bundler.require
 require_relative "../lib/song.rb"
 require_relative "../lib/artist.rb"
 require_relative "../lib/genre.rb"
+#require_relative "../lib/concerns/findable.rb"
 
 module Concerns
 end
--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,32 @@
-class Artist 
-  
+class Artist
+
+  extend Module Concerns::Findable
+
   attr_accessor :name, :songs
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
     save
   end
-  
+
   def self.all
     @@all
-  end 
-  
+  end
+
   def self.destroy_all
     @@all = []
   end
-  
+
   def save
     @@all << self
   end
-  
+
   def self.create(artist_name)
     self.new(artist_name)
     end
-    
+
     def add_song(song)
       #if song.artist != self
       if song.artist == nil
